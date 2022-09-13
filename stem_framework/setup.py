e547f5eba0dd4b3514b2ffcb14c4dd0e6988e92a
Sabina Abdiganieva <abdiganieva.sb@phystech.edu>
2022-09-14 02:33:00 +0300

    fixed a typo in setup.py

--- stem_framework/setup.py
+++ stem_framework/setup.py
@@ -4,7 +4,7 @@ setup(
    name='stem_framework',
    version='0.0.1',
    description='A useful module (maybe)',
-   author='Sabina Abdiganieva,
+   author='Sabina Abdiganieva',
    author_email='ellemgram@gmail.com',
    packages=['stem_framework'],
 )
