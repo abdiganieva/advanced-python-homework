d0fba36cd32a48a535a5e7d9901daabbb936a077
Sabina Abdiganieva <abdiganieva.sb@phystech.edu>
2022-09-14 03:58:48 +0300

    fixed package name in setup.py

--- stem_framework/setup.py
+++ stem_framework/setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
 setup(
-   name='stem_framework',
+   name='stem',
    version='0.0.1',
    description='A useful module (maybe)',
    author='Sabina Abdiganieva',
    author_email='ellemgram@gmail.com',
-   packages=['stem_framework'],
+   packages=['stem'],
 )
