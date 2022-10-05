d2c7ce2919ecad0e82c8d7e5f4d9545638aff4f3
Sabina Abdiganieva <abdiganieva.sb@phystech.edu>
2022-10-06 00:42:12 +0300

    hw for task 3

--- stem_framework/stem/task.py
+++ stem_framework/stem/task.py
@@ -1,6 +1,127 @@
 """
 Pull data flow model is implemented in DataForge by workplaces and tasks. The task execution consists of three steps, namely:
-	1) Task model (the system calculates an acyclic dependency graph of tasks and data).
-	2) Lazy computation model (a ’Goal’ object is created for each specific computation).
-	3) Computation (when the top level goal is triggered, it invokes computation of all goals in chain behind it).
+        1) Task model (the system calculates an acyclic dependency graph of tasks and data).
+        2) Lazy computation model (a ’Goal’ object is created for each specific computation).
+        3) Computation (when the top level goal is triggered, it invokes computation of all goals in chain behind it).
 """
+from typing import TypeVar, Union, Tuple, Callable, Optional, Generic, Any, Iterator
+from abc import ABC, abstractmethod
+from core import Named
+from meta import Specification, Meta
+
+T = TypeVar("T")
+
+
+class Task(ABC, Generic[T], Named):
+    dependencies: Tuple[Union[str, "Task"], ...]
+    specification: Optional[Specification] = None
+    settings: Optional[Meta] = None
+
+    def check_by_meta(self, meta: Meta):
+        pass
+
+    @abstractmethod
+    def transform(self, meta: Meta, /, **kwargs: Any) -> T:
+        pass
+
+
+class FunctionTask(Task[T]):
+    def __init__(self, name: str, func: Callable, dependencies: Tuple[Union[str, "Task"], ...],
+                 specification: Optional[Specification] = None,
+                 settings: Optional[Meta] = None):
+        self._name = name
+        self._func = func
+        self.dependencies = dependencies
+        self.specification = specification
+        self.settings = settings
+
+    def __call__(self, *args, **kwargs):
+        return self._func(*args, **kwargs)
+
+    def transform(self, meta: Meta, /, **kwargs: Any) -> T:
+        return self._func(meta, **kwargs)
+
+
+class DataTask(Task[T]):
+    dependencies = ()
+
+    @abstractmethod
+    def data(self, meta: Meta) -> T:
+        pass
+
+    def transform(self, meta: Meta, /, **kwargs: Any) -> T:
+        return self.data(meta)
+
+
+class FunctionDataTask(DataTask[T]):
+    def __init__(self, name: str, func: Callable,
+                 specification: Optional[Specification] = None,
+                 settings: Optional[Meta] = None):
+        self._name = name
+        self._func = func
+        self.specification = specification
+        self.settings = settings
+
+    def __call__(self, *args, **kwargs):
+        return self._func(*args, **kwargs)
+
+    def data(self, meta: Meta) -> T:
+        return self._func(meta)
+
+
+def data(func: Callable[[Meta], T], specification: Optional[Specification] = None, **settings) -> FunctionDataTask[T]:
+    return FunctionDataTask(func.__name__, func, specification, **settings)
+
+
+
+def task(func: Callable[..., T], specification: Optional[Specification] = None, **settings) -> FunctionTask[T]:
+    return FunctionTask(func.__name__, func, tuple(arg for arg in func.__code__.co_varnames if arg != 'meta'), specification, **settings)
+    
+
+
+class MapTask(Task[Iterator[T]]):
+    def __init__(self, func: Callable, dependence : Union[str, "Task"]):
+        self.func = func
+        
+        if isinstance(dependence, str):
+                self.dependence_name = dependence
+        else:
+                self.dependence_name = dependence.name
+        
+        self._name = 'map_' + self.dependence_name
+    
+    def transform(self, meta: Meta, /, **kwargs: Any) -> T:
+        return map(self._func, *(kwargs.values())) 
+
+
+
+class FilterTask(Task[Iterator[T]]):
+    def __init__(self, key: Callable, dependence: Union[str, "Task"]):
+        
+        self.key = key
+        
+        if isinstance(dependence, str):
+                self.dependence_name = dependence
+        else:
+                self.dependence_name = dependence.name
+        
+        self._name = 'filter_' + self.dependence_name
+        
+    def transform(self, meta: Meta, /, **kwargs: Any) -> T:
+        return filter(self.key, *(kwargs.values())) 
+
+class ReduceTask(Task[Iterator[T]]):
+    def __init__(self, func: Callable, dependence: Union[str, "Task"]):
+        
+        self.func = func
+        
+        if isinstance(dependence, str):
+                self.dependence_name = dependence
+        else:
+                self.dependence_name = dependence.name
+        
+        self._name = 'reduce_' + self.dependence_name
+        
+    
+    def transform(self, meta: Meta, /, **kwargs: Any) -> T:
+        return reduce(self.func, *(kwargs.values())) 
